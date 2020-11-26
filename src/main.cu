fb4f6d03d5fa4d0be3ec98f74a98cd6823aac78e
nvixnu <nvixnu@gmail.com>
2020-11-26 13:50:29 -0300

    main.cu

---
+++ src/main.cu
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : main.cu
+ Author      : Nvixnu
+ Version     :
+ Copyright   : 
+ Description : Programming massively Parallel Processors with CUDA - 3ed.
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void){
+	puts("Hello Parallel World!!!!!");
+
+	return 0;
+}
+
