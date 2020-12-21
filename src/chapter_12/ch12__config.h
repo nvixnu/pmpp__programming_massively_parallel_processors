e0889be3f462c16cf7d4fe87b6d9e3c918411eda
nvixnu <nvixnu@gmail.com>
2020-12-21 08:38:21 -0300

    Adding the chapter 12 skeleton

---
+++ src/chapter_12/ch12__config.h
@@ -0,0 +1,28 @@
+/*
+ * Programming Massively Parallel Processors - 3ed
+ * Chapter 11
+ * Presents the merge sort algorithm and tiling with dynamic input data identification.
+ * The "nvixnu__" libraries used here are available at https://gist.github.com/nvixnu.
+ *
+ *  Created on: 18/12/2020
+ *  Author: Nvixnu
+ */
+
+#ifndef CHAPTER_12_H_
+#define CHAPTER_12_H_
+
+#include "../utils.h"
+#include "../datasets_info.h" //Credit card dataset info
+
+#define CH9__FILEPATH CREDIT_CARD_DATASET_PATH
+#define CH9__ARRAY_LENGTH CREDIT_CARD_DATASET_LENGTH
+
+/**
+ * Performs the Breadth-first search on host and device
+ * @param env Environment to run on (Host or Device)
+ * @param config Kernel configuration parameters such as the block dimension (Number of threads per block)
+ */
+void ch12__bfs(env_e env, kernel_config_t config);
+
+
+#endif /* CHAPTER_12_H_ */
